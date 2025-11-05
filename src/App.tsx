dab3a218626b2eda0ca276949123818505cf2b65
SebastianSPM <sebasperemald04@gmail.com>
2025-11-05 08:40:40 -0500

    Add Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,39 @@ function App() {
 
   return (
     <>
-      <div className='container bg-red-500'>
-        <h1 className='text-black'>ToDo List</h1>
-        <div>
-          <h2>ADD TASK</h2>
-          <button className='bg-[#F44366]'>Add</button>
+      <div className='container bg-[#757DE8] rounded-[5px] p-10'>
+        <h1 className='text-black m-5'>To Do list</h1>
+        <div className='flex justify-center'>
+          <h2 className='text-black m-5 mt-[25px]'>ADD TASK</h2>
+          <button className='bg-[#F44366] text-white font-bold m-5'>ADD</button>
         </div>
         <hr />
         <div>
-          <h2>Task list</h2>
+          <h2 className='text-black m-5'>TASK LIST</h2>
           <ul>
             <li>
-              <h2>Title 1</h2>
-              <p>description</p>
-              <button>Done</button>
-              <button>Delete</button>
+              <div className='flex'>
+              <h2 className='text-black m-5'>Title 1</h2>
+              <p className='text-black m-5'>description</p>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Done</button>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Delete</button>
+              </div>
             </li>
             <li>
-              <h2>Title 2</h2>
-              <p>description</p>
-              <button>Done</button>
-              <button>Delete</button>
+              <div className='flex'>
+              <h2 className='text-black m-5'>Title 2</h2>
+              <p className='text-black m-5'>description</p>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Done</button>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Delete</button>
+              </div>
             </li>
             <li>
-              <h2>Title 3</h2>
-              <p>description</p>
-              <button>Done</button>
-              <button>Delete</button>
+              <div className='flex'>
+              <h2 className='text-black m-5'>Title 3</h2>
+              <p className='text-black m-5'>description</p>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Done</button>
+              <button className='bg-[#F44366] text-white font-bold m-5'>Delete</button>
+              </div>
             </li>
           </ul>
         </div>
